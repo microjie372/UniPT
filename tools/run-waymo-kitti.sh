48f78439ca6cfe0e072892a5816100f227ccf082
microjie372 <92217231+microjie372@users.noreply.github.com>
2024-10-27 17:23:59 +0800

    Update run-waymo-kitti.sh

--- tools/run-waymo-kitti.sh
+++ tools/run-waymo-kitti.sh
@@ -1,5 +1,5 @@
 # pretraining
-bash ./scripts/MDF/dist_train_unipt.sh 8 --cfg_file cfgs/MDF/waymo_kitti/unipt_voxel_rcnn_waymo_kitti --source_one_name waymo
+bash ./scripts/MDF/dist_train_unipt.sh 8 --cfg_file cfgs/MDF/waymo_kitti/unipt_voxel_rcnn_waymo_kitti.yaml --source_one_name waymo
 
 # finetuning
 bash ./scripts/MDF/dist_train_mdf.sh 8 --cfg_file cfgs/MDF/waymo_kitti/waymo_kitti_voxel_rcnn_feat_3_FT.yaml --pretrained_model {your_file_path}--source_one_name waymo 
